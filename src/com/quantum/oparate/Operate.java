d88548ef28e0f8db4057c9c8e3f49a7ec577f3ac
qinleiru <qinleiru@gmail.com>
2018-11-15 11:29:23 +0800

    添加张量积的计算

--- src/com/quantum/oparate/Operate.java
+++ src/com/quantum/oparate/Operate.java
@@ -1,36 +1,86 @@
 package com.quantum.oparate;
 
+import com.quantum.tools.QuantumState;
+
 public class Operate {
-    //张量积的运算
-    //todo：结果正确与否待验证，没有对单粒子之间的张量积做处理
-    public static  double[][] operatorTensor(double[][] operator1,double[][] operator2){
-        int row1=operator1.length;
-        int row2=operator2.length;
-        int col1=operator1[0].length;
-        int col2=operator2[0].length;
-        int row=row1* row2;
-        int col=col1*col2;
-        double[][] result=new double[row][col];
-        for (int i=0;i<row1;i++){
-            for (int j=0;j<col1;j++){
-                for (int k=0;k<row2;k++){
-                    for(int l=0;l<col2;l++){
-                        result[i*row2+k][j*col2+1]=operator1[i][j]*operator2[k][l];
+    //张量积的运算,矩阵之间的张量积，因为会量子门的操作也会使用张量积
+    public static double[][] operatorTensor(double[][] A, double[][] B) {
+
+        final int m = A.length;
+        final int n = A[0].length;
+        final int p = B.length;
+        final int q = B[0].length;
+        double[][] result = new double[m * n][p * q];
+        for (int i = 0; i < m; i++) {
+
+            final int iOffset = i * p;
+            for (int j = 0; j < n; j++) {
+
+                final int jOffset = j * q;
+                final double aij = A[i][j];
+
+                for (int k = 0; k < p; k++) {
+
+                    for (int l = 0; l < q; l++) {
+
+                        result[iOffset + k][jOffset + l] = aij * B[k][l];
+
                     }
+
                 }
+
+
             }
+
         }
         return result;
     }
-    public static void main(String[] args){
-        double [][] op1=new double[][]{{2,0},{-1,1}};
-        double [][] op2=new double[][]{{1,2,3},{1,0,1}};
-        double [][] result=Operate.operatorTensor(op1,op2);
-        for (int i=0;i<result.length;i++){
-            for(int j=0;j<result[0].length;j++) {
-                System.out.print(result[i][j]+" ");
+    //对量子态进行幺正操作
+//    public static void performOperator(QuantumState state,int pos,double[][] operator){
+//        double [][] targetState
+//    }
+
+    //向量之间的张量积运算用于计算量子系统的状态
+    public static double[]operatorTensor(double[]A, double[]B) {
+
+        final int m = A.length;
+        final int p = B.length;
+        double[]result = new double[m * p];
+        int k=0;
+        for (int i = 0; i < m; i++) {
+
+            final int iOffset = i * p;
+            for (int j = 0; j < p; j++) {
+                result[k]=A[i]*B[j];
+                k++;
+
             }
-            System.out.println();
+
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args){
+        //向量张量积运算的例子
+        double []op1=new double[]{1,0};
+        double []op2=new double[]{0,1};
+        double [] result=Operate.operatorTensor(op1,op2);
+        for (int i=0;i<result.length;i++){
+                System.out.print(result[i]+"        ");
+        }
+        System.out.println("矩阵张量积");
+        System.out.println("");
+
+        //矩阵之间张量积运算的例子
+        double [][]op3=new double[][]{{1,0},{0,1}};
+        double [][]op4=new double[][]{{0,1},{1,0}};
+        double [][]result1=Operate.operatorTensor(op3,op4);
+        for (int i=0;i<result1.length;i++){
+            for(int j=0;j<result1[0].length;j++){
+                System.out.print(result1[i][j]+"        ");
+            }
+            System.out.println("");
         }
     }
 }
